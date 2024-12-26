b3499757620f75b7c1b6dc97f7f79ffd99846d22
Wiquzix <ctac.nep@gmail.com>
2024-12-26 22:26:04 +0700

    add README req and edit main

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ app.include_router(router_user)
 
 
 if __name__=="__main__":
-    uvicorn.run("main:app",reload=True)
+    uvicorn.run("main:app",host="0.0.0.0", port=8000,reload=True) #хост и порт для докера
     # uvicorn это ASGI-сервер нужен для запуска FastAPI приложений (что бы не писать каждый раз "uvicorn main:app --reload" в консоли а использовать обычный python main.py так удобнее будет для запуска в контейнере)
 
